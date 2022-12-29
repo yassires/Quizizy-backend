26d577fb5d0a9272661ad5f074b29b858eae78a7
yassir <yas.esraidi@gmail.com>
2022-12-29 17:26:54 +0100

    start

---
+++ script.js
@@ -0,0 +1,196 @@
+let DATA;
+let j;
+fetch("qz.json")
+    .then((data) => data.json())
+    .then((data) => {
+        DATA = data.quiz[0];
+        j = DATA.questions.sort(()=>Math.random() - 0.5);
+    })
+var time = 0;
+var interval;
+let index = 0;
+let seconds;
+const next_btn = document.getElementById("submit");
+const start_btn = document.getElementsByClassName("start");
+let elements  = document.getElementsByClassName("answer");
+
+
+// start
+start_btn[0].addEventListener("click", function (e) {
+    clearInterval(interval);
+    afficher_question(DATA.questions[index]);
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].addEventListener("click", function (e) {
+          elements[i].classList.toggle("active");
+        });
+      }
+    index++;
+})
+
+
+// submit
+next_btn.addEventListener("click", function (e) {
+
+    check()
+    endQuiz(index)
+    clearInterval(interval);
+   
+    afficher_question(DATA.questions[index]);
+    // check()
+
+     for (let i = 0; i < elements.length; i++) {
+        elements[i].addEventListener("click", function (e) {
+            for (let j = 0; j< elements.length; j++) {
+                
+                  elements[j].classList.remove("active");
+                
+              }
+          elements[i].classList.add("active");
+        });
+      }
+    index++;
+})
+
+
+
+// end 
+
+function endQuiz(index){
+    if(index>=6){
+        
+        document.querySelector('.card').style.display='none'
+        document.querySelector('.card2').style.display='block'
+        next_btn.remove()
+        clearInterval(interval);
+        ver()
+        console.log(list);
+
+    }
+    return;
+}
+
+function timer() {
+     seconds = 30;
+
+// timer 
+    if (seconds > 0) {
+        interval = setInterval(() => {
+            document.getElementById("seconds").innerHTML=seconds+" Seconds" ;
+            seconds--;
+
+            if (seconds == 0) {
+                clearInterval(interval);
+                index++;
+                afficher_question(DATA.questions[index]);
+                
+            }
+        }, 1000);
+    } else {
+
+    }
+}
+
+
+
+let test;
+function afficher_question(question) {
+    test = question.content;
+    timer();
+
+    console.log(question)
+   
+    let output = `
+            <div> 
+                <div class="quiz">
+                    <h3>Question ${interval}/6</h3>
+                    <h5 class="seconds" id="seconds"></h5>
+                </div>
+                <div class="quiz-area">
+                <h2>${question.content}</h2>
+                </div>
+            <div class="answers-area">
+                <div class="answer" >
+                    <label for="answer-1">${question.options[0].content}</label> 
+                    <input type="radio" id="answers" name="questions" value="${question.options[0].content}"/>
+                </div>
+                <div class="answer" >
+                    <label for="answer-2">${question.options[1].content}</label>
+                    <input type="radio" id="answers" name="questions" value="${question.options[1].content}"/>
+                </div>
+                <div class="answer" >
+                    <label for="answer-3">${question.options[2].content}</label> 
+                    <input type="radio" id="answers" name="questions" value="${question.options[2].content}"/>
+                </div>
+                <div class="answer" >
+                    <label for="answer-4" >${question.options[3].content}</label> 
+                    <input type="radio" id="answers" name="questions" value="${question.options[3].content}"/>
+                </div>
+            </div>
+            </div>
+            
+    
+    `;
+    document.querySelector("#question").innerHTML = output;
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const quizSection = document.querySelector(".card");
+    const resultsSection = document.querySelector(".card2");
+    const startQuizButton = document.querySelector(".start");
+    const informationSection = document.querySelector(".card3");
+  
+    quizSection.style.display = "none";
+    resultsSection.style.display = "none";
+
+    startQuizButton.addEventListener("click", function() {
+      quizSection.style.display = "";
+      informationSection.style.display = "none";
+    });
+  });
+
+  let list = []
+  function check(){
+    let val
+    let checks = document.getElementsByName('questions')
+        for (let i = 0; i < checks.length; i++) {
+            // console.log(checks[i].value)
+            if(checks[i].checked){
+                val = checks[i].value
+                //console.log(val)
+                list.push(val)  
+                console.log(list);
+            }
+        }
+  }
+
+let score=0;
+  function ver(){
+    for (let i = 0; i < list.length; i++) {
+        console.log(list[i])
+        console.log(DATA.questions[i].answer.correct)
+        if(list[i]===DATA.questions[i].answer.correct){
+            console.log('yes')
+            score++;
+        }else{
+            console.log('no');
+            let result = document.querySelector("#result");
+            result.innerHTML += `
+                                <div class="resultSection"> 
+                                <h2>${DATA.questions[i].content}</h2> 
+                                <p class="userch">${list[i]}</p>
+                                <p class="correctAnswer">${DATA.questions[i].answer.correct}</p> <br>
+                                <p class="justify">${DATA.questions[i].answer.comment}</p> <br>
+                                <hr>
+                                </div>
+                                
+                                `
+            // result.innerHTML += '<h2>'+DATA.questions[i].content+'</h2>'
+            // result.innerHTML += '<p class="userch">'+list[i]+'</p>'
+            // result.innerHTML += '<p class="correctAnswer">' + DATA.questions[i].answer.correct +'</p> <br>'
+            // result.innerHTML += '<p class="justify">' + DATA.questions[i].answer.comment +'</p> <br>'
+        }
+    }
+  }
+
+  
---
+++ view/index.php
@@ -0,0 +1,86 @@
+<?php
+ 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link href="https://fonts.googleapis.com/css2?family=Karla:wght@300&display=swap" rel="stylesheet">
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+    />
+    <!-- CSS only -->
+    <title>Quizizy™</title>
+</head>
+<body>
+    <nav>
+        <img src="../img/aws.png" style="width: 200px;" alt="Logo">
+    </nav>
+
+        <div class="card">
+                    <div class="icon">
+                        <i class="bi bi-cloud">   Aws - Quiz</i>
+                    </div>
+                    <div id="question">
+                       
+                    </div>
+            
+            <div class="btnSubmit">
+                <button type="submit" id="submit">
+                    Next
+                </button>
+            </div>
+            
+        </div>
+        <div class="card3">
+            <div class="icon">
+                <i class="bi bi-cloud">   Aws - Quiz</i>
+            </div>
+                <div class="button-container">
+                    <div class="pd">
+                        <div id="note" class="container col-md-6 shadow p-3 mb-5 bg-white rounded">
+                            <h1>AWS Cloud Practitioner Knowledge Test</h1>
+                            <h5>Try to answer the following questions in given time limit<br/>
+                            Keep in mind that incorrect answer will penalize time/score
+                            </h5>
+                        </div>
+                        <div id="start">
+                            <input type="text" id="yn" placeholder="Enter your name" />
+                        </div>
+                        <div id="start">
+                            <button class="start">Let's Goo</button>
+                        </div>
+                    </div>
+                  
+                  
+                  <!-- <a href="" id="start">Let's Goo</a> -->
+                </div>
+        </div>
+        <div class="card2">
+            <div class="icon">
+                <i class="bi bi-cloud">   Aws - Quiz</i>
+            </div>
+
+            <div class="title">
+                <h1>Your wrong answers</h1>
+            </div>
+            <div id="result">
+            </div>
+            <div class="btnSubmit">
+                <button type="submit" id="submit">
+                     Next
+                </button>
+            </div>
+    
+        </div>
+        
+</body>
+<script src="../script.js" defer></script>
+
+</html>
+
